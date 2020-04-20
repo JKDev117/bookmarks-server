bea3c55ee672f3bc10d957ca32dcd5f0bc1f8dc2
Jae Kim <jckim81@yahoo.com>
2020-04-20 15:44:56 -0400

    modified store.js

--- src/store.js
+++ src/store.js
@@ -4,7 +4,17 @@ const bookmarks = [{
     "url": "https://www.google.com",
     "description": "Where we find everything else",
     "rating": 4
-}]
+},
+{
+    "id": 2,
+    "title": "Bing",
+    "url": "https://www.bing.com",
+    "description": "Bing helps you turn information into action, making it faster and easier to go from searching to doing.",
+    "rating": 4
+}
+
+
+]
 
 
 
