0d644ab5e39810adff81e631bfba9c6631302129
Jae Kim <jckim81@yahoo.com>
2020-05-04 11:04:06 -0400

    v5-4-20 Mon 11:03AM

--- src/app.js
+++ src/app.js
@@ -20,7 +20,9 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+/*
 app.use(validateBearerToken);
+*/
 
 app.use(function errorHandler(error, req, res, next) {
   let response
